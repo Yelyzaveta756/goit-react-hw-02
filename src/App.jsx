d697de4b4ef3639053de9c2043ce524fa7fc4cf8
Liza <selanta19@gmail.com>
2024-05-20 13:58:35 +0300

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import Options from "./components/Options/Options.jsx"
 import Feedback from './components/Feedback/Feedback.jsx';
+import Notification from './components/Notification/Notification.jsx'
+import Description from './components/Description/Description.jsx'
 import { useState, useEffect } from "react";
-
-import css from "./components/Feedback/Feedback.module.css"
-
 
 export default function App () {
 
@@ -12,11 +11,8 @@ const [options, setOptions] = useState(()=> {
   return saveOptions !== null ? JSON.parse(saveOptions) : { good: 0, neutral: 0, bad: 0 };
 })
 
-const [hasFeedback, setHasFeedback] = useState(false);
-
 useEffect(() => {
   window.localStorage.setItem("saved-options", JSON.stringify(options));
-  setHasFeedback(options.good > 0 || options.neutral > 0 || options.bad > 0);
 }, [options])
 
 const updateFeedback = feedbackType => {
@@ -39,19 +35,18 @@ const totalFeedback = options.good + options.neutral + options.bad;
 
 const positiveFeedback = totalFeedback > 0 ? Math.round((options.good / totalFeedback) * 100) : 0;
 
-
+const showReset = totalFeedback > 0;
 
     return (
         <>
-        <h1>Sip Happens Café</h1>
-        <p>Please leave your feedback about our service by selecting one of the options below.</p>
+        <Description />
         <Options 
         onClick={updateFeedback}
         onReset={resetFeedback}
-        showReset={hasFeedback}
+        showReset={showReset}
         />
          {totalFeedback == 0 ? ( 
-         <p>No feedback yet</p>
+         <Notification />
         ) : ( <Feedback 
           goodFeedback={options.good}
           badFeedback={options.bad}
